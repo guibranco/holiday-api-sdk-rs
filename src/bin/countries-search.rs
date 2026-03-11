04751dc59dd4cf03e3ee9f9303a61fb0fd754013
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2026-03-11 12:20:06 +0000

    Update countries-search.rs

--- src/bin/countries-search.rs
+++ src/bin/countries-search.rs
@@ -1,24 +1,23 @@
-extern crate dotenv;
-extern crate holiday_api_rust;
-#[macro_use]
-extern crate prettytable;
-
 use dotenv::dotenv;
 use holiday_api_rust::HolidayAPIClient;
 use prettytable::Table;
 use prettytable::format;
 use std::env;
 
-fn main(){
+#[macro_use]
+extern crate prettytable;
+
+#[tokio::main]
+async fn main() {
     dotenv().ok();
 
     let api_key = env::var("HOLIDAYAPI_APIKEY").unwrap();
-    
+
     let client = HolidayAPIClient::new(api_key);
 
-    match client.search_countries(){
+    match client.search_countries().await {
         Err(e) => eprintln!("{:?}", e),
-        Ok(countries) =>{
+        Ok(countries) => {
             let mut table = Table::new();
             table.set_format(*format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
             table.set_titles(row!["Code", "Name", "Flag"]);
@@ -26,6 +25,7 @@ fn main(){
             for country in countries {
                 table.add_row(row![country.code, country.name, country.flag]);
             }
+
             table.printstd();
         }
     }
